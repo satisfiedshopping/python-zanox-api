7b04c739f17cf497c4de028b3c7d90448d30646c
Bas Koopmans <e-mail@baskoopmans.nl>
2014-08-27 10:48:42 +0200

    Enhanced destination url extractor and added tests

--- zanox/apis.py
+++ zanox/apis.py
@@ -7,21 +7,19 @@ import base64
 import random
 import string
 import json
-from urlparse import urlparse
 
+from urlparse import urlparse, parse_qs
 from urllib import urlencode
+
+from zanox import __version__
 
 import requests
 import xmltodict
 
 
-def random_string(length, characters=string.ascii_uppercase + string.ascii_lowercase + string.digits):
-    return ''.join(random.choice(characters) for character in range(length))
-
-
 class PublisherApi(object):
 
-    def __init__(self, connect_id, secret_key, domain='api.zanox.com', format='json', version='2011-03-01', ssl=True, *args, **kwargs):
+    def __init__(self, connect_id, secret_key, domain='api.zanox.com', format='json', version='2011-03-01', ssl=True, user_agent=None, from_email=None, *args, **kwargs):
         self.connect_id = connect_id
         self.secret_key = secret_key
         self.ssl = ssl
@@ -31,6 +29,8 @@ class PublisherApi(object):
         self.format = format
         self.datetime_format = '%a, %d %b %Y %H:%M:%S GMT' # example: Thu, 15 Aug 2013 15:56:07 GMT
         self.session = None
+        self.user_agent = user_agent or "PythonZanoxApi/{0}".format(__version__)
+        self.from_email = from_email
 
         # Go through keyword arguments, and either save their values to the instance
         for key, value in kwargs.iteritems():
@@ -55,40 +55,58 @@ class PublisherApi(object):
         uri = url_parts.path.split(self.version)[-1]
         return uri
 
-    @staticmethod
-    def extract_destination_url_from_tracking_url(tracking_url, clean=False):
-        response = requests.head(tracking_url, allow_redirects=True)
+    def extract_destination_url_from_tracking_url(self, tracking_url, clean=False):
+        headers = self.get_default_headers()
+        response = requests.head(tracking_url, allow_redirects=True, headers=headers)
+
+        # Adfarm has jasvascript/html redirecting and returns 200 status code
+        # Let's extract the real destination url from de url
+        destination_url = response.url
+        if 'adfarm.mediaplex.com' in response.url:
+            query = urlparse(destination_url).query
+            destination_url = query.split('mpro=')[-1]
+
+        # Strip the query parameters full of tracking stuff
         if clean:
-            url_parts = urlparse(response.url)
+            url_parts = urlparse(destination_url)
             destination_url = '{0}://{1}{2}'.format(url_parts.scheme, url_parts.netloc, url_parts.path)
-        else:
-            destination_url = response.url
         return destination_url
 
     def get_signature(self, url, method, date, nonce):
-        # construct signature
+        # Construct signature
         uri = self.extract_uri_from_url(url)
         date = date.strftime(self.datetime_format)
         method = method.upper()
         elements = (method, uri, date, nonce)
         signature = u''.join(elements)
 
-        # encode signature SHA256, and Base 64
+        # Encode signature SHA256, and Base64
         signature = hmac.new(self.secret_key, msg=signature, digestmod=hashlib.sha1).digest()
         signature = base64.b64encode(signature)
         signature = '%s:%s' % (self.connect_id, signature)
         return signature
 
-    def get_request_headers(self, url, method):
+    def get_default_headers(self):
+        headers = {'User-Agent': self.user_agent}
+        if self.from_email:
+            headers['From'] = self.from_email
+        return headers
+
+    @staticmethod
+    def generate_nonce(length=32, characters=string.ascii_uppercase + string.ascii_lowercase + string.digits):
+        return ''.join(random.choice(characters) for item in range(length))
+
+    def get_authenticated_headers(self, url, method):
         # format datetime
         date = datetime.datetime.utcnow()
-        nonce = random_string(32)
+        nonce = self.generate_nonce()
         signature = self.get_signature(url, 'GET', date, nonce)
-        headers = {
+        headers = dict(self.get_default_headers())
+        headers.update({
             'Authorization': "ZXWS {0}".format(signature),
             'Date': date.strftime(self.datetime_format),
             'nonce': nonce,
-        }
+        })
         return headers
 
     @staticmethod
@@ -102,10 +120,11 @@ class PublisherApi(object):
 
     def get(self, resource, **parameters):
         url = self.construct_url(resource, **parameters)
-        headers = self.get_request_headers(url, method='GET')
+        headers = self.get_authenticated_headers(url, method='GET')
         response = requests.get(url, headers=headers)
-        json = response.json()
-        return json
+        if self.format == 'json':
+            return response.json()
+        return response
 
     def get_program_identifier(self, tracking_url):
         return tracking_url.lower().split('&ulp')[0].split('ppc/?')[1]
@@ -113,7 +132,8 @@ class PublisherApi(object):
     def get_tracking_url(self, destination_url, adspace_id):
         """Get a tracking url for a given destination url and adspace id"""
         deeplink_api_url = '{0}://toolbox.zanox.com/tools/api/deeplink?connectid={1}&adspaceid={2}&url={3}'.format(self.protocol, self.connect_id, adspace_id, destination_url)
-        response = requests.get(deeplink_api_url)
+        headers = self.get_default_headers()
+        response = requests.get(deeplink_api_url, headers=headers)
         tracking_url = xmltodict.parse(response.text)['deeplink']['url']
         return tracking_url
 
---
+++ zanox/test.py
@@ -0,0 +1,52 @@
+# encoding: utf-8
+
+import unittest2 as unittest
+
+from .apis import PublisherApi
+
+
+class ExtractDestinationUrlTest(unittest.TestCase):
+
+    def setUp(self):
+        self.api = PublisherApi(connect_id='802B8BF4AE99EBE00F41', secret_key='fa4c0c2020Aa4c+ab9Ea0ec8d39E06/df2c5aa44')
+
+    def test_normal(self):
+        tracking_url = 'http://ad.zanox.com/ppc/?25738162C98353673&amp;ULP=[[http://www.manfield.com/product/09111600?channel_code=74&amp;product_code=62164779&amp;utm_campaign=sales&amp;utm_content=prfeed&amp;utm_medium=affiliate&amp;utm_source=Zanox]]&amp;zpar9=[[9010F8C4AD9955DBDC9F]]'
+        destination_url = 'http://www.manfield.com/product/09111600'
+        self.assertEqual(self.api.extract_destination_url_from_tracking_url(tracking_url, clean=True), destination_url)
+
+    def test_without_http(self):
+        tracking_url = 'http://ad.zanox.com/ppc/?25695976C44878398&amp;ULP=[[www.hardloopschoenencenter.nl/product/434398]]&amp;zpar9=[[9010F8C4AD9955DBDC9F]]'
+        destination_url = 'http://www.hardloopschoenencenter.nl/product/434398/nike-storm-slim-waistpack.html'
+        self.assertEqual(self.api.extract_destination_url_from_tracking_url(tracking_url, clean=True), destination_url)
+
+    def test_with_extra_url(self):
+        tracking_url = 'http://ad.zanox.com/ppc/?25712131C50023437&amp;ULP=[[http://adfarm.mediaplex.com/ad/ck/25522-185146-14530-4?destination=Productfeed&amp;mpro=http%3A%2F%2Fwww.esprit.nl%2Fkids%2Fbaby-0-2-jaar%2Fbaby-boys%2Fjeans-broeken%2Fjeans-%26-broeken-074EEBB008_406%3Fcamp%3DNL_IC_ZX_AF_20_001]]&amp;zpar9=[[9010F8C4AD9955DBDC9F]]'
+        destination_url = 'http://www.esprit.nl/kids/baby-0-2-jaar/baby-boys/jeans-broeken/jeans-&-broeken-074EEBB008_406'
+        self.assertEqual(self.api.extract_destination_url_from_tracking_url(tracking_url, clean=True), destination_url)
+
+    def test_path_only(self):
+        tracking_url = 'http://ad.zanox.com/ppc/?25738046C20244849&amp;ULP=[[cd/this-et-al/baby-machine/8609983.html?affil=zanox]]&amp;zpar9=[[9010F8C4AD9955DBDC9F]]'
+        destination_url = 'http://www.zavvi.nl/cd/this-et-al/baby-machine/8609983.html'
+        self.assertEqual(self.api.extract_destination_url_from_tracking_url(tracking_url, clean=True), destination_url)
+
+    def test_path_only2(self):
+        tracking_url = 'http://ad.zanox.com/ppc/?25719026C47878862&amp;ULP=[[/4+paar+verschillende+katoenrijke+sokken/P22323198,nl_NL,pd.html]]&amp;zpar9=[[9010F8C4AD9955DBDC9F]]'
+        destination_url = 'http://www.marksandspencer.eu/4-paar-verschillende-katoenrijke-sokken/P22323198,nl_NL,pd.html'
+        self.assertEqual(self.api.extract_destination_url_from_tracking_url(tracking_url, clean=True), destination_url)
+
+    def test_id_only(self):
+        tracking_url = 'http://ad.zanox.com/ppc/?25737964C93517905&amp;ULP=[[031662900?channel_code=74&amp;product_code=71491142&amp;utm_campaign=sales&amp;utm_content=prfeed&amp;utm_medium=affiliate&amp;utm_source=Zanox]]&amp;zpar9=[[9010F8C4AD9955DBDC9F]]'
+        destination_url = 'http://www.scapino.nl/product/031662900'
+        self.assertEqual(self.api.extract_destination_url_from_tracking_url(tracking_url, clean=True), destination_url)
+
+
+
+
+
+
+
+
+
+
+
